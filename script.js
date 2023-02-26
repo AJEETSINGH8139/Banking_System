f1bb65a4359c1763142335fc78f01ac57d272a7d
AJEET SINGH <ajeet.singh.cs23@iilmcet.ac.in>
2023-02-26 17:16:29 +0530

    Some editing in style

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const account2 = {
 };
 
 const account3 = {
-  owner: 'Neha Shukla',
+  owner: 'Neha Shukla Singh',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
